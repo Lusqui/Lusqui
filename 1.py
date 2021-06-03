b2f1a929b4f114bd3fc5d1dba3a4d0628c8c5955
Lusqui <84420400+Lusqui@users.noreply.github.com>
2021-06-03 05:33:40 +0200

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-speed = float(input("Whats is the speed? "))
-if speed <=70:
-        print("Ok")
-else:
-    points = str((speed - 70) // 5)
-    print("Points: " + points)
-    if float(points) >=12:
-        print("License suspended")
